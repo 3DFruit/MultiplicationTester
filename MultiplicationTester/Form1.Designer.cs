e15aa76b2c66a869eee9ba4d7a1caa53289a6195
Чиркин Иван <fruitin3d@yandex.ru>
2020-04-07 12:58:33 +0700

    Вернул заголовок

--- MultiplicationTester/Form1.Designer.cs
+++ MultiplicationTester/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.Text = "Умножение столбиком";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
