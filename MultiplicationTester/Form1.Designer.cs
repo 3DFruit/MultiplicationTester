f05f599fe20803a7eee212a381af09fc1958747e
Чиркин Иван <fruitin3d@yandex.ru>
2020-04-07 12:50:31 +0700

    Сделано

--- MultiplicationTester/Form1.Designer.cs
+++ MultiplicationTester/Form1.Designer.cs
@@ -29,31 +29,20 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
             this.button = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.mainPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel1
+            // labelOutput
             // 
-            this.panel1.Controls.Add(this.numericUpDown1);
-            this.panel1.Location = new System.Drawing.Point(13, 64);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(87, 117);
-            this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(196, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 24);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Умножение столбиком";
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelOutput.Location = new System.Drawing.Point(196, 21);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(216, 24);
+            this.labelOutput.TabIndex = 0;
+            this.labelOutput.Text = "Умножение столбиком";
             // 
             // button
             // 
@@ -62,54 +51,39 @@
             this.button.Name = "button";
             this.button.Size = new System.Drawing.Size(177, 49);
             this.button.TabIndex = 1;
+            this.button.TabStop = false;
             this.button.Text = "Проверить";
             this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
             // 
-            // numericUpDown1
+            // mainPanel
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown1.Location = new System.Drawing.Point(3, 85);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            10000,
-            0,
-            0,
-            0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(73, 29);
-            this.numericUpDown1.TabIndex = 2;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.numericUpDown1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            8888,
-            0,
-            0,
-            0});
+            this.mainPanel.Location = new System.Drawing.Point(13, 65);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(463, 199);
+            this.mainPanel.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(488, 276);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.labelOutput);
             this.Controls.Add(this.button);
-            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Умножение столбиком";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOutput;
         private System.Windows.Forms.Button button;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
 
--- MultiplicationTester/Form1.cs
+++ MultiplicationTester/Form1.cs
@@ -16,9 +16,149 @@ namespace MultiplicationTester
             InitializeComponent();
         }
 
+        List <int[]> sums;
+        List<Panel> panels;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            numericUpDown1.Controls[0].Visible = false;
+            labelOutput.Text = "";
+            GenerateSums(10);
+            Display();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if(b.Text == "Проверить")
+            {
+                int rightAnswersCount = 0;
+                for (int i = 0; i < sums.Count; i++)
+                {
+                    int answer = int.Parse(panels[i].Controls["numeric"].Text);
+                    if (answer == sums[i][0] * sums[i][1])
+                    {
+                        rightAnswersCount++;
+                        panels[i].BackColor = Color.LightGreen;
+                        panels[i].Controls["rtb"].BackColor = Color.LightGreen;
+                    }
+                    else
+                    {
+                        panels[i].BackColor = Color.IndianRed;
+                        panels[i].Controls["rtb"].BackColor = Color.IndianRed;
+                    }
+                    panels[i].Controls["numeric"].Enabled = false;
+                }
+                labelOutput.Text = "Правильных ответов " + rightAnswersCount + " из " + sums.Count;
+                button.Text = "Начать заново";
+            }
+            else
+            {
+                labelOutput.Text = "";
+                button.Text = "Проверить";
+                GenerateSums(10);
+                Display();
+            }
+        }
+
+        private void GenerateSums(int count)
+        {
+            sums = new List<int[]>();
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int[] sum = new int[2];
+                do
+                {
+                    int hundreds = random.Next(1, 10);
+                    int tens;
+                    int units;
+                    if (random.NextDouble() <= 0.5)
+                    {
+                        tens = random.Next(1, 10);
+                        units = 0;
+                    }
+                    else
+                    {
+                        tens = 0;
+                        units = random.Next(1, 10);
+                    }
+                    sum[0] = hundreds * 100 + tens * 10 + units;
+                    sum[1] = random.Next(2, 10);
+                }
+                while (sums.Contains(sum));
+                sums.Add(sum);
+            }
+        }
+
+        private void SelectAllOnEnter(object sender, EventArgs e)
+        {
+            (sender as NumericUpDown).Select(0, this.ActiveControl.ToString().Length);            
+        }
+
+        private void NextTabOnReturnOrSpace(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return || e.KeyCode == Keys.Space)
+            {
+                e.SuppressKeyPress = true;
+                this.SelectNextControl(this.ActiveControl, true, true, true, true);
+            }
+        }
+
+        private void Display()
+        {
+            panels = new List<Panel>();
+            mainPanel.Controls.Clear();
+            for (int i = 0; i < sums.Count; i++)
+            {
+                Panel panel = new Panel();
+                panel.Location = new Point(5 + 90 * (i % 5), 5 + 97 * (i / 5)); 
+                panel.Size = new Size(87, 91);
+                panel.Name = "panel" + i;
+                panel.BorderStyle = BorderStyle.FixedSingle;
+                Font font = new Font("Microsoft Sans Serif", 14);
+                RichTextBox rtb = new RichTextBox();
+                rtb.Location = new Point(25, 5);
+                rtb.Size = new Size(57, 47);
+                rtb.ReadOnly = true;
+                rtb.BackColor = panel.BackColor;
+                rtb.Font = font;
+                rtb.Text = sums[i][0].ToString() + "\n         " + sums[i][1].ToString();
+                rtb.Name = "rtb";
+                rtb.TabStop = false;
+                rtb.SelectAll();
+                rtb.SelectionAlignment = HorizontalAlignment.Right;
+                rtb.DeselectAll();
+                rtb.Select(rtb.Text.IndexOf('\n'), rtb.Text.Length - rtb.Text.IndexOf('\n'));
+                rtb.SelectionFont = new Font("Microsoft Sans Serif", 14, FontStyle.Underline);
+                rtb.DeselectAll();
+                rtb.BorderStyle = BorderStyle.None;
+                rtb.ScrollBars = RichTextBoxScrollBars.None;
+                Label label = new Label();
+                label.Text = "x";
+                label.Font = font;
+                label.AutoSize = true;
+                label.Location = new Point(5, 18);
+                NumericUpDown numeric = new NumericUpDown();
+                numeric.Font = font;
+                numeric.TextAlign = HorizontalAlignment.Right;
+                numeric.UpDownAlign = LeftRightAlignment.Left;
+                numeric.Value = 0;
+                numeric.Maximum = 10000;
+                numeric.Size = new Size(77, 29);
+                numeric.Location = new Point(5, 56);
+                numeric.Name = "numeric";
+                numeric.Enter += new EventHandler(SelectAllOnEnter);
+                numeric.KeyDown += new KeyEventHandler(NextTabOnReturnOrSpace);
+                numeric.Click += new EventHandler(SelectAllOnEnter);
+                panel.Controls.Add(rtb);
+                panel.Controls.Add(label);
+                panel.Controls.Add(numeric);
+                panels.Add(panel);
+            }
+            foreach (Panel panel in panels)
+            {
+                mainPanel.Controls.Add(panel);
+            }
         }
     }
 }
